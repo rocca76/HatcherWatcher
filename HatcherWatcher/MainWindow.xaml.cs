5e5686237903c33605c08a433bcfeb1b8727ed9c
eric.aubin <eric.aubin@olympusndt.com>
2013-03-15 17:24:31 -0400

    StartListener

--- HatcherWatcher/MainWindow.xaml.cs
+++ HatcherWatcher/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System;
 
 namespace HatcherWatcher
 {
@@ -19,9 +11,40 @@ namespace HatcherWatcher
   /// </summary>
   public partial class MainWindow : Window
   {
+    private TcpListener _listener;
+    private Thread _listenerThread;
+
     public MainWindow()
     {
       InitializeComponent();
+      StartListener();
+    }
+
+    private void StartListener()
+    {
+      _listenerThread = new Thread(RunListener);
+      _listenerThread.Start();
+    }
+
+    private void RunListener()
+    {
+      _listener = new TcpListener(IPAddress.Any, 11000);
+      _listener.Start();
+      while (true)
+      {
+        TcpClient client = _listener.AcceptTcpClient();
+
+        this.Dispatcher.BeginInvoke((Action)(() => { textBoxLog.Text += string.Format("\nNew connection from {0}", client.Client.RemoteEndPoint); }));
+
+        ThreadPool.QueueUserWorkItem(ProcessClient, client);
+      }
+    }
+
+    private void ProcessClient(object state)
+    {
+      TcpClient client = state as TcpClient;
+      // Do something with client
+      // ...
     }
   }
 }
